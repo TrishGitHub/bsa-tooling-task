c4c5aedf5bec86a627a6a556316155d7ba4f7d93
TrishGitHub <i.noschenko@gmail.com>
2016-07-17 22:07:39 +0300

    tasks done

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var jade = require('gulp-jade');
+var sass = require('gulp-sass');
 var rjs = require('gulp-requirejs');
-// add required packages
+var uglify = require('gulp-uglify');
 
 gulp.task('connect', function() {
 	connect.server({
@@ -20,7 +21,11 @@ gulp.task('jade', function() {
 });
 
 gulp.task('sass', function() {
-	// implement sass task
+	return gulp.src('src/sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
+	.pipe(connect.reload());
+	
 });
 
 gulp.task('requireJS', function() {
@@ -37,9 +42,17 @@ gulp.task('requireJS', function() {
 	.pipe(connect.reload());
 });
 
+gulp.task('minify', function () {
+    gulp.src('dist/js/bundle.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/min'));
+});
+
 gulp.task('watch', function() {
 	gulp.watch('src/jade/*.jade', ['jade']);
+	gulp.watch('src/sass/*.sass', ['sass']);
+	gulp.watch('src/js/*.js', ['requireJS']);
 	// add watch for .sass and .js files
 });
 
-gulp.task('default', ['requireJS', 'jade', 'sass', 'connect', 'watch']);
+gulp.task('default', ['requireJS', 'minify', 'jade', 'sass', 'connect', 'watch']);
